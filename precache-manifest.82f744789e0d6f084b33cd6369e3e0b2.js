a103c9010bb673926f2e8036f4ba9b1ef4b98821
Jose Cortés <josecortesdavila@gmail.com>
2020-02-24 17:38:53 +0100

    Upate Demo Page 🆕

---
+++ precache-manifest.82f744789e0d6f084b33cd6369e3e0b2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f5f7ff69621d272c45c73a340b5bfba",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "5394ed54c4b507c1988d",
+    "url": "/tic-tac-toe/static/css/main.5312e462.chunk.css"
+  },
+  {
+    "revision": "56bc61909fa69d46d970",
+    "url": "/tic-tac-toe/static/js/2.0dce989c.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/tic-tac-toe/static/js/2.0dce989c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5394ed54c4b507c1988d",
+    "url": "/tic-tac-toe/static/js/main.07e3bfed.chunk.js"
+  },
+  {
+    "revision": "e5d7aca4491c82ee1a95",
+    "url": "/tic-tac-toe/static/js/runtime-main.33bcfadf.js"
+  }
+]);
